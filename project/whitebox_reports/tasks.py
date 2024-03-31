6336042c72a7450e0011ee4ab06613019dd58c72
gk07 <gk_520@hotmail.com>
2024-03-31 17:50:40 +0800

    report files volume

--- project/whitebox_reports/tasks.py
+++ project/whitebox_reports/tasks.py
@@ -1,4 +1,5 @@
 import random
+import pathlib
 
 import requests
 from asgiref.sync import async_to_sync
@@ -6,6 +7,10 @@ from celery import shared_task
 from celery.signals import task_postrun
 from celery.utils.log import get_task_logger
 from project.database import db_context
+
+from project.whitebox_reports.doc_report.doc_data import TemplateInfo
+from project.whitebox_reports.doc_report.render import DocReportRenderer
+from project.whitebox_reports.doc_report.doc_data import DocDataItem, EmbedFileInfo, DocDataItemType
 
 logger = get_task_logger(__name__)
 
@@ -18,6 +23,15 @@ def start_code_trusty(search_version: str, group_name: str, b_version: str):
 @shared_task
 def start_code_metric(search_version: str, group_name: str, b_version: str):
     logger.info(f"start_code_metric: {search_version}, {group_name}, {b_version}")
+
+@shared_task
+def make_doc_report():
+    tpl_fn = "tests/fixtures/doc_report_template_text.docx"
+    output_fn = "report_files/doc_report_output_text.docx"
+    tpl_info = TemplateInfo(file_path=pathlib.Path(tpl_fn))
+    render = DocReportRenderer(tpl_info, output_fn)
+    doc_data = [DocDataItem(name="test_text", type=DocDataItemType("text"), value="张三\n李四\n王五"),]
+    render.render(doc_data)
 
 
 whitebox_report_task_dict = {
--- project/whitebox_reports/views.py
+++ project/whitebox_reports/views.py
@@ -1,17 +1,10 @@
-from typing import List, Optional
-import random
-
-from asgiref.sync import async_to_sync
-from celery import shared_task
-from celery.signals import task_postrun
-from celery.utils.log import get_task_logger
-from project.database import db_context
 
 from . import whitebox_reports_router
-from .tasks import start_code_trusty, start_code_metric
+from .tasks import make_doc_report
 
 
 
-@whitebox_reports_router.post("/start-project")
-def start_project(search_version: str, group_name: str, b_version: str, check_options: Optional[List[str]] = None):
-    return {"search_version": search_version, "group_name": group_name, "b_version": b_version, "check_options": check_options}
+@whitebox_reports_router.post("/doc-report")
+def start_project():
+    task = make_doc_report.delay()
+    return {"task_id": task.id}
